5607577260232f1c6fb87d721d3dcf251a3a4010
salmvenera <ahmetovavm@gmail.com>
2021-10-22 16:31:55 +0500

    13 урок сделано

--- js/script.js
+++ js/script.js
@@ -5,42 +5,53 @@ const headerInput = document.querySelector('.header-input')
 const todoList = document.querySelector('.todo-list')
 const todoCompleted = document.querySelector('.todo-completed')
  
-const toDoData = []
+let toDoData = localStorage.getItem('todo') ? JSON.parse(localStorage.getItem('todo')) : [];
+
+/*
+let toDoData = localStorage.getItem('todo') ? JSON.parse(localStorage.getItem('todo')) : [];
+if(localStorage.getItem('todo'))
+    toDoData = JSON.parse(localStorage.getItem('todo'))
+else
+    toDoData = []
+*/
  
 const render = function() {
     todoList.innerHTML = ''
     todoCompleted.innerHTML = ''
     
-
-    toDoData.forEach(function(item) {
+    toDoData.forEach(function(item,index) {
+        
+        //localStorage.setItem("todo", JSON.stringify(toDoData)) 
        const li = document.createElement('li')
        li.classList.add('todo-item')
        li.innerHTML = '<span class="text-todo">' + item.text + '</span>' +
            '<div class="todo-buttons">' +
            '<button class="todo-remove"></button>' +
 	       '<button class="todo-complete"></button>' +
-           '</div>'
+           '</div>';
 
         if(item.completed) {
             todoCompleted.append(li)
         } else {
             todoList.append(li)
         }
+        localStorage.setItem("todo", JSON.stringify(toDoData))
         li.querySelector('.todo-complete').addEventListener('click', function() {
             item.completed = !item.completed
             render()
         })
         li.querySelector('.todo-remove').addEventListener('click', function() {
             li.remove()   
+            toDoData.splice(index, 1);
+            localStorage.setItem("todo", JSON.stringify(toDoData))
+            render();
         })
-        localStorage.setItem("ToDo", JSON.stringify(toDoData))
-        
-    })
+    })    
 }
 
 todoControl.addEventListener('submit', function(event) {
-    event.preventDefault()
-
+    event.preventDefault();
+    if(headerInput.value === '') return false;
     const newToDo = {
         text: headerInput.value,
         completed: false
@@ -48,6 +59,8 @@ todoControl.addEventListener('submit', function(event) {
 
     toDoData.push(newToDo)
     headerInput.value = ''
-    render() 
+    render();
 })
+
+render();
 
